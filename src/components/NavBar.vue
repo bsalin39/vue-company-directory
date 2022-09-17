e2e50a9c3dd80ed32361c92b191f937d22503295
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-09-16 19:49:16 -0500

    added navigation layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref('🏢 Fake Company Directory')
+</script>
+
+<template>
+  <nav>
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+
+      <div class="menu">
+        <a href="#" class="menu-item">Departments</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-item">Logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-20 bg-slate-900 text-slate-200;
+
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+
+      .brand {
+        &-title {
+          @apply text-2xl font-bold text-red-600;
+        }
+      }
+
+      .menu {
+        @apply flex gap-2;
+
+        &-item {
+          @apply hover: rounded-md bg-yellow-500 px-4 py-2 hover:text-slate-900;
+        }
+
+        &-login {
+          @apply hover: rounded-md bg-blue-500 bg-blue-700 px-4 py-2 text-red-100;
+        }
+
+        &-logout {
+          @apply hover: mx-2 rounded-md bg-gray-500 bg-gray-700 px-4 py-2 text-red-100;
+        }
+      }
+    }
+  }
+</style>
