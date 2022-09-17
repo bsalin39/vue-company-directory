e8fec5a795b52234b77b34f7a081f4f4c0d31128
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-09-16 23:44:02 -0500

    created pagination

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,7 +13,7 @@
       <div class="menu">
         <a href="#" class="menu-item">Departments</a>
         <a href="#" class="menu-item">Settings</a>
-        <a href="#" class="menu-item">Logout</a>
+        <button class="menu-logout" @click="logout">Logout</button>
       </div>
     </div>
   </nav>
@@ -22,29 +22,26 @@
 <style scoped lang="postcss">
   nav {
     @apply flex h-20 bg-slate-900 text-slate-200;
-
     .wrapper {
       @apply container mx-auto flex w-full items-center justify-between;
-
       .brand {
         &-title {
-          @apply text-2xl font-bold text-green-600;
+          @apply text-2xl font-bold text-green-500;
         }
       }
-
       .menu {
         @apply flex gap-2;
-
+        & div {
+          @apply py-2;
+        }
         &-item {
-          @apply rounded-md bg-yellow-400 py-2 px-4 font-bold text-slate-500 hover:text-slate-900;
+          @apply rounded-md px-4 py-2 hover:bg-blue-400 hover:text-slate-900;
         }
-
         &-login {
-          @apply rounded-md bg-blue-500 px-4 py-2 text-red-100;
+          @apply rounded-md bg-green-500 px-4 py-2 text-red-100 hover:bg-green-700;
         }
-
         &-logout {
-          @apply mx-2 rounded-md bg-gray-500 px-4 py-2 text-red-100;
+          @apply mx-2 rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
         }
       }
     }
